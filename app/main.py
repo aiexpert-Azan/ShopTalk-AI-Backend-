9ff224963486940a00c485e647295668d9cd3def
aiexpert.Azan <aiexpert.azan@gmail.com>
2026-02-23 01:53:05 +0500

    error resolved

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins for testing; restrict in production
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
